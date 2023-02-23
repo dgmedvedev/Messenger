33fd1338beddf3f779f7ca023dc56351411d3a90
dgmedvedev <dg-medvedev@mail.ru>
2023-02-23 15:34:40 +0300

    Completed project

--- app/src/main/java/demo/com/CreateMessageActivity.java
+++ app/src/main/java/demo/com/CreateMessageActivity.java
@@ -18,12 +18,23 @@ public class CreateMessageActivity extends AppCompatActivity {
 
         editTextMsg = findViewById(R.id.editTextMessage);
         Button buttonSend = findViewById(R.id.buttonSend);
+        Button buttonSendOut = findViewById(R.id.buttonSendOut);
 
         buttonSend.setOnClickListener(view -> {
             String msg = editTextMsg.getText().toString();
             Intent intent = new Intent(this, ReceivedMessageActivity.class);
             intent.putExtra("message", msg); // вложили в intent данные
             startActivity(intent);
+        });
+
+        buttonSendOut.setOnClickListener(view -> {
+            String msg = editTextMsg.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, msg); // вложили в intent данные
+            // дополнительный intent, чтобы невозможно было по умолчанию выбрать приложение
+            Intent chosenIntent = Intent.createChooser(intent, getString(R.string.chooser_title));
+            startActivity(chosenIntent);
         });
     }
 }
